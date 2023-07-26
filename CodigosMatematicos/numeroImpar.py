b6dee4582fabb2fb236a269983014f11b5eac6e5
martin coronado <45699940+martinheler@users.noreply.github.com>
2023-07-26 09:57:23 -0500

    Borrar archivos temporales

--- CodigosMatematicos/numeroImpar.py
+++
@@ -1,7 +0,0 @@
-def pruebaImpar(numero):
-    if (numero%2==0):
-        return "{} es un numero par".format(numero)
-    else:
-        return "{} es un numero impar".format(numero)
-    
-print(pruebaImpar(2))
